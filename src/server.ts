bc3551ad796ff2bdb71291576f06fcf79e87b0bf
Elizeu Lopes <scarabellot.lopes@gmail.com>
2025-01-23 18:05:07 -0300

    feat: add route settings

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import cors from "cors";
+
+import { router } from "./routes";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
